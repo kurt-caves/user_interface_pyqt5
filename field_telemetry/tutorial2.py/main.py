82a11427e8d8e46b258b038a4d246f20a2132547
kurtly <kurtly@nmsu.edu>
2024-11-01 15:59:41 -0600

    working on tutorial

---
+++ field_telemetry/tutorial2.py/main.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+
+'''
+https://www.youtube.com/watch?v=RxGlB9U64fg&list=PLs3IFJPw3G9KhF7BeGOItwoKKLD8e3Dwu&index=7
+'''
